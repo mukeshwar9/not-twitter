4baf71c7940cec0020febb266247cead809ceeb9
mukeshwar9 <mukeshwar1998@gmail.com>
2023-08-26 12:16:40 +0530

    Added posts route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { SignIn, UserButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
+import { api } from "~/utils/api";
 
 export default function Home() {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
   const user = useUser();
   return (
     <>
@@ -14,6 +16,11 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {!user.isSignedIn && <SignIn />}
         {user.isSignedIn && <UserButton afterSignOutUrl="/" />}
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
